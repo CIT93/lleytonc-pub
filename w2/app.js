98d0f7ed35620de84d5932890203092cdfc825af
Lleyton Craig <lleyton.k.craig@gmail.com>
2026-01-22 18:33:23 -0800

    Step 2.3 Complete

--- w2/app.js
+++ w2/app.js
@@ -15,20 +15,20 @@ const itemPrice = 15;
 const handleButtonClick = function() {
     // clickCount = clickCount + 1;
     // Increase clickCount by 1 each time the button is clicked
-    totalCost += 1;
+    totalCost += itemPrice;
 
     // Template strings (literal) to easily combine our variables and text into one message
-    let message = `Hello, You have clicked the button ${totalCost} time(s).`;
+    let message = `Current Total: $${totalCost}`;
 
     // This is basic decision-making in JavaScript!
     // Use a simple 'if' statement to make our page react differently based on clickCount.
-    if(totalCost >= 5) {
+    if(totalCost >= 60) {
         // We can even change the style of an HTML element directly with JavaScript!
         // Change text color
-        message += ' WOW, you are a super clicker!';
-        totalDisplayElement.style.color = 'purple';
+        message += ' (Over Budget!)';
+        totalDisplayElement.style.color = 'red';
     } else {
-        totalDisplayElement.style.color = '#333';
+        totalDisplayElement.style.color = 'green';
     }
     // Update the text content of our paragraph element on the page.
     // This is how JavaScript makes changes visible on the web page!
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log('DOM fully loaded and parsed, App is ready for interaction')
     // Attach an event listener to our 'updateButton.
     // When 'updateButton' receives a 'click' event, the 'handleButtonClick' function will execute.
-    addItem.addEventListener('click', handleButtonClick);
+    updateButton.addEventListener('click', handleButtonClick);
     
     
 })
