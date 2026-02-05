4c911f6bdc05c59ef7c8c68e0b3366932b8d1ed4
Lleyton Craig <lleyton.k.craig@gmail.com>
2026-02-05 12:26:12 -0800

    Step 3 Complete

--- w4/app.js
+++ w4/app.js
@@ -1,14 +1,27 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
 
 import * as orderForm from "./order-handler.js";
+import * as priceCalculator from './price-calculator.js';
+
 
 const orderFormElement = document.getElementById("order-form");
-const orderSummary = document.getElementById("order-summary");
+//const orderSummary = document.getElementById("order-summary");
+
+const orders = [];
 
 const handleOrderSubmit = function(event) {
     event.preventDefault();
    const orderData = orderForm.getOrderInputs();
-   orderSummary.textContent = (`Ordered ${orderData.qty} ${orderData.size} T-shirts`);
+   const priceData = priceCalculator.calculateTotal(orderData);
+
+   const newOrder = {
+    ...orderData,
+    ...priceData,
+    timestamp: new Date().toISOString()
+   }
+
+   orders.push(newOrder);
+   console.log(orders);
 };
 
 const init = function () {
