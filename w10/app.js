6b89fcb4f6e2fa241e88970901af8df67aa903de
Lleyton Craig <lleyton.k.craig@gmail.com>
2026-03-19 16:01:51 -0700

    w10 setup done

---
+++ w10/app.js
@@ -0,0 +1,65 @@
+console.log('Hello from app.js! Your JavaScript is connected and running!');
+
+import * as orderForm from "./order-handler.js";
+import * as priceCalculator from './price-calculator.js';
+import * as orderStorage from './order-storage.js';
+import * as orderList from './order-list.js';
+
+const orderFormElement = document.getElementById("order-form");
+//const orderSummary = document.getElementById("order-summary");
+
+const orders = [];
+
+const handleOrderSubmit = function (event) {
+    event.preventDefault();
+    const orderData = orderForm.getOrderInputs();
+    const priceData = priceCalculator.calculateTotal(orderData);
+
+    const newOrder = {
+        id: Date.now().toString(),
+        ...orderData,
+        ...priceData,
+        timestamp: new Date().toISOString()
+    };
+
+    orders.push(newOrder);
+
+    orderStorage.saveOrders(orders);
+
+    orderList.renderOrders(orders, {
+        onDelete: handleDelete,
+        onEdit: handleEdit
+    });
+};
+
+const handleClearForm = function () {
+    orderForm.clearForm();
+    console.log('reset button clicked');
+};
+
+const handleDelete = function (id) {
+    console.log("App.js: Requesting delete for order", id);
+};
+
+const handleEdit = function (id) {
+    console.log("App.js: Requesting edit for order", id);
+};
+
+
+const init = function () {
+    console.log("App Initialized");
+    const loadedOrders = orderStorage.loadOrders();
+    if (loadedOrders.length > 0) {
+        orders.push(...loadedOrders);
+        console.log('Orders loaded');
+
+        orderList.renderOrders(orders, {
+            onDelete: handleDelete,
+            onEdit: handleEdit
+        });
+    }
+    orderFormElement.addEventListener("submit", handleOrderSubmit);
+    orderFormElement.addEventListener("reset", handleClearForm);
+};
+
+document.addEventListener("DOMContentLoaded", init)
---
+++ w10/order-list.js
@@ -0,0 +1,66 @@
+// order-list.js top of file
+let moduleCallbacks = {};
+
+// Date
+const formatDateForDisplay = function (timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    });
+};
+
+const createTableRow = function (order) {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${formatDateForDisplay(order.timestamp)}</td>
+    <td>${order.qty}</td>
+    <td>${order.size}</td>
+    <td>${order.totalPrice}</td>
+    <td>
+        <button class="edit-btn" data-id="${order.id}">Edit</button>
+        <button class="delete-btn" data-id="${order.id}">Delete</button>
+    </td>
+    `;
+
+    return row;
+};
+
+const orderTableBody = document.getElementById('order-table-body')
+
+orderTableBody.addEventListener('click', function(event) {
+    const target = event.target;
+    
+    // 1. Get the ID from the button that was clicked
+    const id = target.dataset.id;
+
+    // 2. Guard Clause: If they clicked a row (white space) but NOT a button, 
+    // there will be no ID. So we stop the function immediately.
+    if (!id) return;
+ 
+    if(target.classList.contains('delete-btn')) {
+        moduleCallbacks.onDelete(id);
+    } else if(target.classList.contains("edit-btn")) {
+        moduleCallbacks.onEdit(id);
+    }
+});
+
+export const renderOrders = function (orders, callbacks) {
+    moduleCallbacks = callbacks;
+    orderTableBody.innerHTML = '';
+    console.log('inside orderList')
+
+    if (orders.length === 0) {
+        return;
+    }
+
+
+    for (const order of orders) {
+        console.log(`${order}`)
+        const rowElement = createTableRow(order);
+        orderTableBody.appendChild(rowElement);
+    }
+
+};
